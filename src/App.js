bc9492b9c868734bea842c8d9ba73446dd780741
gpp <peshopeshopesho1234567@gmail.com>
2019-02-23 19:57:30 +0200

    changed app.js text again

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Does this github/travis-ci workflow work?
+            Does github/travis-ci workflow work?
           </a>
         </header>
       </div>
